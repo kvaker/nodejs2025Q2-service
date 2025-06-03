a52f9f0170d44ec9f074b0d7962252ac09c43ff6
Vitali Kuntsevich <kvadesstudio@gmail.com>
2025-06-03 17:45:36 +0300

    feat: add TypeOrmModule and .entity to user/artist/track/album/favorites

--- src/track/track.module.ts
+++ src/track/track.module.ts
@@ -1,10 +1,12 @@
 import { Module, forwardRef } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TrackController } from './track.controller';
 import { TrackService } from './track.service';
 import { ArtistModule } from 'src/artist/artist.module';
+import { Track } from './track.entity';
 
 @Module({
-  imports: [forwardRef(() => ArtistModule)],
+  imports: [TypeOrmModule.forFeature([Track]), forwardRef(() => ArtistModule)],
   controllers: [TrackController],
   providers: [TrackService],
   exports: [TrackService],
